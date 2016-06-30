e93e4386ff86bbeae116a10d1525edd90fbcbdb7
Margaret Knoblock <mrk7217@gmail.com>
2016-06-30 11:11:43 -0700

    got fileRead working. can now read in a csv file (aka our spreadsheet) and will print out whatever column specified. now that we know how to access sections of the spreadsheet we need to use wget to open files.

--- src/FileRead.java
+++ src/FileRead.java
@@ -1,11 +1,36 @@
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+
+import org.junit.Test;
 
 import com.opencsv.CSVReader;
 
 public class FileRead {
-	String csvFilename = new String();
-	File newFile = new File(csvFilename);
-	FileReader read = new FileReader(newFile); //need to figure out how to fix thissss
-	CSVReader csvReader = new CSVReader(read);
+	private final String csvFilename = "/Users/margaretknoblock/Downloads/otherLic.csv";
+	List<String[]> fullFile = null;
+	String[] tempRow = null;
+	final int licenseRow = 5;
+	final int licenseDetailedRow = 6;
+	final int urlRow = 7;
+	 
+	@Test
+	public void accessFile(){
+		try{
+			FileReader read = new FileReader(csvFilename); //need to figure out how to fix thissss
+			CSVReader csvReader = new CSVReader(read);
+			fullFile = csvReader.readAll();
+			for(int i=1;i<10;i++){ //starts at one to skip the titles of each column
+				tempRow = (String[]) fullFile.get(i);
+				System.out.println(tempRow[licenseRow]); //access only the desired column
+				
+			}
+			csvReader.close();
+		} 
+		catch(IOException ex){	
+			ex.printStackTrace();
+		}
+	}
 }
