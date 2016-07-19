0d31caab31afc13209f7379ae3270f4dfd0a830b
Margaret Knoblock <mrk7217@gmail.com>
2016-07-19 12:59:04 -0700

    integrated wget into update license class. have a bug in the when not url same or updated license

--- src/UpdateLicense.java
+++ src/UpdateLicense.java
@@ -1,11 +1,15 @@
 import org.junit.Test;
 
+import java.io.File;
+import java.nio.file.*;
+
 //to run through a csv sheet and update the licenses by comparing url 
 //and file contents as well as user input when the system can not self-determine the updated license
 public class UpdateLicense { 
-	private final String csvFilename = "/Users/margaretknoblock/Desktop/moreTest.csv"; //csv file to read in
+	private final String csvFilename = "/Users/margaretknoblock/Downloads/otherLic.csv"; //csv file to read in
 	FileRead fileReader = new FileRead();
 	FileWrite fileWriter = new FileWrite();
+	WGet fileGetter = new WGet();
 	int currentRow = 1; //cant start at row zero because titles are there
 	String currentLicense; 
 	String [][] updatedSheetInfo; //creates empty 2d string array for all the updated sheet information
@@ -21,14 +25,17 @@ public class UpdateLicense {
 		fileReader.openFile(csvFilename); 
 		init();
 		checkSameLicense();
+		
 		fileReader.closeFile(); //should put at reached end once done testing
 		writeLicense(); //writes to new spreadsheet
+		
+		
 	}
 	
 	public void checkSameLicense(){ //checks to see if the two rows have the same license
 		if (fileReader.accessFile(currentRow+1, updatedLicenseCol).isEmpty()){ //checks to see if second url already has a license
-			if (fileReader.accessFile(currentRow,urlCol) == fileReader.accessFile(currentRow+1, urlCol)){ //if urls for first and second row are the same
-				addRowNewLice(fileReader.getRow(currentRow+1)); //add the row with the new license
+			if (fileReader.accessFile(currentRow,urlCol).equals(fileReader.accessFile(currentRow+1, urlCol))){ //if urls for first and second row are the same
+				addRowNewLice(currentRow+1); //add the row with the new license
 				nextURL(); //move to next row
 			}
 			else{ // urls not same so checks license detailed
@@ -40,19 +47,28 @@ public class UpdateLicense {
 				String ul2 = fileReader.accessFile(currentRow+1, licenseDetailedCol);
 				//checks to see if they are both pom files and have the same license detailed column (that isn't other)
 				if (((urlEnd1.equals("pom")) && (urlEnd2.equals("pom"))) && (!fileReader.accessFile(currentRow, licenseDetailedCol).equals("Other")) && (ul1.equals(ul2))){
-					addRowNewLice(fileReader.getRow(currentRow+1)); //if same license detailed adds row with the new license
+					addRowNewLice(currentRow+1); //if same license detailed adds row with the new license
 					nextURL(); //move to next row 
 				}
 				else{ //need to open files to compare
 					System.out.println("Not same url or same detailed");
-					addRow(fileReader.getRow(currentRow+1), currentRow+1); //delete after actually adding in wget
+					addRow(currentRow+1); //delete after actually adding in wget
+					
+					//String file1 = getFile(currentRow); //creates file and saves full path name as a string
+					//String file2 = getFile(currentRow + 1);
+					/*if (true){ //fileComp(file1, file2)
+						addRowNewLice(currentRow+1);
+					}
+					else{
+						addRow(currentRow+1);
+					}*/
 					//need to do wget and file comparison
-					nextURL();
+					nextURL(); //problems here when i run with full openlic spreadsheet
 				}
 			}
 		}
 		else{ //if second url already has a license, moves to next one
-			addRow(fileReader.getRow(currentRow+1), currentRow+1); //add the second row without changing the license
+			addRow(currentRow+1); //add the second row without changing the license
 			currentLicense = fileReader.accessFile(currentRow+1, updatedLicenseCol); //changes current license to what is there
 			nextURL(); //moves to next row
 		}
@@ -61,16 +77,24 @@ public class UpdateLicense {
 	public void init(){ //initializes updated sheet info, sets current license and puts in title row of updatedSheetInfo
 		updatedSheetInfo = new String[fileReader.numRows()][fileReader.numCols()];
 		currentLicense = fileReader.accessFile(currentRow, updatedLicenseCol);
-		addRow(fileReader.getRow(0),0); //adds title row to new spreadsheet
-		addRow(fileReader.getRow(1),1); //adds first row to new spreadsheet (assuming first url already has a license)
+		addRow(0); //adds title row to new spreadsheet
+		addRow(1); //adds first row to new spreadsheet (assuming first url already has a license)
 	}
 	
-	public void addRow(String[] row, int loc){ //if no license change
+	public String getFile(int row){ //creates file from url and returns the path of the file as a string
+		File newFile = fileGetter.getFile(fileReader.accessFile(row, urlCol));
+		String filePath = fileGetter.getFilePath(newFile);
+		return filePath;
+	}
+	
+	public void addRow(int rowNum){ //if no license change
+		String [] row = fileReader.getRow(rowNum); //gets row from file
 		for(int i=0;i<updatedSheetInfo[0].length;i++) //adds all elements of row to updated sheet info
-			updatedSheetInfo[loc][i] = checkCommas(row[i]); //makes sure no commas are in there
+			updatedSheetInfo[rowNum][i] = checkCommas(row[i]); //makes sure no commas are in there
 	}
 	
-	public void addRowNewLice(String[] row){ //if the license changes
+	public void addRowNewLice(int rowNum){ //if the license changes
+		String [] row = fileReader.getRow(rowNum); //gets row from file 
 		for(int i=0;i<updatedSheetInfo[0].length;i++) //adds all elements of row to updated sheet info
 			updatedSheetInfo[currentRow+1][i] = checkCommas(row[i]);
 		updatedSheetInfo[currentRow+1][updatedLicenseCol] = currentLicense; //changes license column to the updated license
@@ -98,7 +122,7 @@ public class UpdateLicense {
 			checkSameLicense(); //checks licenses
 		}
 		else { //if at end, adds last row
-			addRow(fileReader.getRow(currentRow+1), currentRow+1); //assuming last row will have an updated license
+			addRow(currentRow+1); //assuming last row will have an updated license
 			System.out.println("Reached end of spreadsheet");
 		}
 	}
--- src/WGet.java
+++ src/WGet.java
@@ -2,26 +2,36 @@ import java.io.*;
 import java.net.*;
  
 public class WGet { //for saving files from a given url
-  public static void main(String[] args) throws Exception {
-    String s;
-    String urlName = "https://repo1.maven.org/maven2/cglib/cglib-nodep/2.2/cglib-nodep-2.2.pom";
-    BufferedReader r = new BufferedReader(new InputStreamReader(new URL(urlName).openStream()));
+	public File getFile(String urlName) {
+		try{
+			String s;
+			BufferedReader r = new BufferedReader(new InputStreamReader(new URL(urlName).openStream()));
     
-    String fileName = urlName.substring(urlName.lastIndexOf('/') + 1); //makes name of file the final section of the url after the last "/"
-    String fileLoc = "files/"; //sets location for file to be saved
-    String fileDest = fileLoc + fileName; //sets full file name with destination included
-    File file = new File(fileDest); // creates file
+			String fileName = urlName.substring(urlName.lastIndexOf('/') + 1); //makes name of file the final section of the url after the last "/"
+			String fileLoc = "files/"; //sets location for file to be saved
+			String fileDest = fileLoc + fileName; //sets full file name with destination included
+			File file = new File(fileDest); // creates file
     
-    //may be an issue with similar or identical urls, need to add program control for these possibilities
-    //ask to double check ^^
-    if (!file.exists()){ //should add else for possibility of same file--would have same end extension
-    	PrintWriter writer = new PrintWriter(file);
-    	while ((s = r.readLine()) != null) { //while lines are not null
-    		writer.println(s); //writes lines to the file
-    	}
-    	writer.close(); //closes file writer
-    }
-    
-    
-  }
+			if (!file.exists()){ //will not write file again if file already exists
+				PrintWriter writer = new PrintWriter(file);
+				while ((s = r.readLine()) != null) { //while lines are not null
+					writer.println(s); //writes lines to the file
+				}
+				writer.close(); //closes file writer
+			}
+			return file;
+		}
+		catch(IOException ex){
+			ex.printStackTrace();
+			return null;
+		}
+	}  
+	
+	public String getFilePath(File file){
+		String filePath = file.getAbsolutePath();
+		return filePath;
+	}
+  
+  
 }
+
